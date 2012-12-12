c6cdbef33dcb73b5a4831347edd489ea688de7cb
Cody Stoltman <particlebanana@gmail.com>
2012-12-11 20:17:50 -0600

    simple express server with no routes

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+    path = require('path'),
+    express = require('express');
+
+var app = express();
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('production', function() {
+  app.use(express.logger('short'));
+});
+
+app.configure('development', function() {
+  app.use(express.logger('dev'));
+  app.use(express.errorHandler());
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
