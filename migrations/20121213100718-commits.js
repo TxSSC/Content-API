ab098ab878e95cfa92d1486c7ad75195dbb463d5
Matt McFarland <matmcfarland@gmail.com>
2012-12-13 11:04:45 -0600

    add migration for commits

---
+++ migrations/20121213100718-commits.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: function(migration, DataTypes) {
+    migration.createTable('commits', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      item_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    },
+    {
+      engine: 'MYISAM', // default: 'InnoDB'
+      charset: 'latin1' // default: null
+    });
+
+    // Foreign Key Index
+    migration.addIndex('commits', ['item_id', 'user_id']);
+  },
+  down: function(migration) {
+    migration.dropTable('commits');
+  }
+};
