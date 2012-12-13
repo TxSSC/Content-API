0000a963cf060df2e2a76c548ce7a4c329203053
Cody Stoltman <particlebanana@gmail.com>
2012-12-12 20:53:46 -0600

    add a helpers tasks to drop and rebuild the test database

---
+++ test/v1/support/helpers.js
@@ -0,0 +1,19 @@
+/*
+ * Helper Tasks
+ */
+
+module.exports = function(sequelize) {
+  return {
+    Up: function(cb) {
+      sequelize.sync().success(function() {
+	return cb();
+      });
+    },
+
+    Down: function(cb) {
+      sequelize.drop().success(function() {
+	return cb();
+      });
+    }
+  };
+};
