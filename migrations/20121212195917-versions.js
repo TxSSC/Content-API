ae12ae24d603ed9ae415a4b1aa09da3fd91c0f2e
Cody Stoltman <particlebanana@gmail.com>
2012-12-12 20:51:51 -0600

    add versions migration

---
+++ migrations/20121212195917-versions.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: function(migration, DataTypes) {
+    migration.createTable('versions', {
+      id: {
+	type: DataTypes.INTEGER,
+	primaryKey: true,
+	autoIncrement: true
+      },
+      topic_id: {
+	type: DataTypes.INTEGER,
+	allowNull: false
+      },
+      name: {
+	type: DataTypes.STRING,
+	allowNull: false
+      },
+      published: {
+	type: DataTypes.BOOLEAN,
+	allowNull: false,
+	defaultValue: false
+      },
+      updated_at: {
+	type: DataTypes.DATE,
+	allowNull: false
+      },
+      created_at: {
+	type: DataTypes.DATE,
+	allowNull: false
+      }
+    },
+    {
+      engine: 'MYISAM', // default: 'InnoDB'
+      charset: 'latin1' // default: null
+    });
+
+    // Foreign Key Index
+    migration.addIndex('versions', ['topic_id']);
+  },
+  down: function(migration) {
+    migration.dropTable('versions');
+  }
+};
